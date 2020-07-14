7bf49cc10b983738ed8a6973a63f998c5f243ed3
Sriram <vinaysriramvsr007@gmail.com>
2020-07-14 12:44:46 +0530

    Updates

---
+++ precache-manifest.13d74b6ce871e0db074ef9061fcd818d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479893dbb100c123c253a88777ef33a2",
+    "url": "/taskreact/index.html"
+  },
+  {
+    "revision": "ab524c73be738928f0af",
+    "url": "/taskreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b3aa87efab9f39b12ea",
+    "url": "/taskreact/static/js/2.fceb65b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskreact/static/js/2.fceb65b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab524c73be738928f0af",
+    "url": "/taskreact/static/js/main.8a4a21c4.chunk.js"
+  },
+  {
+    "revision": "490bdb4caa0b1ebb7834",
+    "url": "/taskreact/static/js/runtime-main.876ff27e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taskreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
